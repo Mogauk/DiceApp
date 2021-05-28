2fb3a833cff2ec1c0887adc066e212d4c573a076
Alexey <line6@mail.ru>
2021-05-28 20:43:13 +1100

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Alexey on 5/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
